fbaee17332d4949cd0f74affc8bd605467ce4392
Ivan <mad.genius69@gmail.com>
2023-09-28 17:10:58 +0300

    28.09

--- дз/3/app.js
+++ дз/3/app.js
@@ -39,22 +39,85 @@
 
 
 //dz 3
-var temp = 0;
-function isPerfectNumber(number){
-    for(var i=1;i<=number/2;i++) {
-        if(number%i === 0) {
-           temp += i;
-         }
-    }
-    if(temp === number && temp !== 0) {
-      return true;
-       }  
-     else {
-      return false;
-    } 
-}
+// var temp = 0;
+// function isPerfectNumber(number){
+//     for(var i=1;i<=number/2;i++) {
+//         if(number%i === 0) {
+//            temp += i;
+//          }
+//     }
+//     if(temp === number && temp !== 0) {
+//       return true;
+//        }  
+//      else {
+//       return false;
+//     } 
+// }
 
-console.log(isPerfectNumber(6));   // true
-console.log(isPerfectNumber(28));  // true
-console.log(isPerfectNumber(12));  // false
-console.log(isPerfectNumber(16));  // false
+// console.log(isPerfectNumber(6));   // true
+// console.log(isPerfectNumber(28));  // true
+// console.log(isPerfectNumber(12));  // false
+// console.log(isPerfectNumber(16));  // false
+
+// dz 4
+// const car1 =  document.getElementById('car');
+
+// let car = {
+//   name: 'Hendai Solaris',
+// 	type: 'sedan',
+// 	maxSpeed: '320',
+// 	color: 'red',
+// }
+
+// function cars (){
+//   return `
+//   <article>
+//   <div>
+//       <h2>${car.name}</h2>
+//       <span>${car.type}</span>
+//   </div>
+//   <div>
+//       <span>Maximum speed:${car.maxSpeed}</span>
+//   </div>
+//   <div>
+//       <span>Color:${car.color}</span>
+//   </div>
+// </article>`
+// }
+
+// car1.insertAdjacentHTML('beforeend',cars(car));
+
+
+//dz 5 
+
+// const sentence = "Hello, how are you?";
+
+// function countWords(){
+//   return sentence.split('').length;
+// }
+
+// console.log(countWords());
+
+
+//dz 6 
+
+// function generateGoogleString(oCount) {
+//   // Проверяем, чтобы количество букв "o" было не менее 2
+//   if (oCount < 2) {
+//     return "Google";
+//   }
+
+//   // Создаем строку с заданным количеством "o"
+//   const oString = "o".repeat(oCount);
+
+//   // Возвращаем строку "Google" с добавленными "o" по краям
+//   return `G${oString}gle`;
+// }
+
+// // Примеры использования:
+// console.log(generateGoogleString(2)); // "Google"
+// console.log(generateGoogleString(4)); // "Gooooogle"
+// console.log(generateGoogleString(-2));
+
+// dz 7 
+
